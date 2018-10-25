5d37d51c417f67722c2d69c11f851a9f50fa8d9c
Rudy <rudyegall@gmail.com>
2018-10-25 17:20:04 -0400

    Book get/set and book list

--- Book.cs
+++ Book.cs
@@ -69,3 +69,15 @@ namespace Midterm
         }
     }
 }
+Book In SearchofLostTime = new Book("In Search of Lost Time", "Marcel Proust");
+Book DonQuixote = new Book("Don Quixote", "Miguel de Cervantes");
+Book Ulysses = new Book("Ulysses", "James Joyce");
+Book TheGreatGatsby = new Book("The Great Gatsby", "F.Scott Fitzgerald");
+Book MobyDick = new Book("Moby Dick", "Herman Melville");
+Book Hamlet = new Book("Hamlet", "William Shakespeare");
+Book WarandPeace = new Book("War and Peace", "Leo Tolstoy");
+Book TheOdyssey = new Book("The Odyssey", "Homer");
+Book Lolita = new Book("Lolita", "Vladimir Nabokov");
+Book OntheRoad = new Book("On the Road", "Jack Kerouac");
+Book Breakfastofchampions = new Book("Breakfast of champions", "Kurt Vonnegut");
+Book TheAlchemist = new Book("The Alchemist", "Paulo Coelho");
